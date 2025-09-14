691175ab72f6767ffe75d00ee011bdfe9fcda936
Kazutech1 <umehemmanuel22@gmail.com>
2025-09-14 17:12:50 +0100

    cors fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS everywhere
+  app.enableCors({
+    origin: '*',
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   
